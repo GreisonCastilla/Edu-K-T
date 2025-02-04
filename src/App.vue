7b7e5b55401a498b06f01e4fd5dd7a8aaca634ab
Greison <grisoncastilla@gmail.com>
2025-02-04 18:05:51 -0500

    add sideBar function

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     import CardCourse from './components/Cardcourse/CardCourse.vue';
     import Indicator from './components/Indicator.vue';
     import data from './components/Cardcourse/courses.json'
-    import grade from './grade.json'
     import Title from './components/Title.vue';
     import SideBar from './components/SideBar.vue';
     import Hero from './components/Hero.vue';
 
+    import { actualGrade } from './composables/useGrades';
     import { showSideBar } from './composables/useSideBar';
 
 </script>
@@ -26,12 +26,12 @@
             :class="{' md:relative md:-translate-x-44 transition duration-500 ease-in-out min-w-full':!showSideBar}"
             >
                 <div >
-                    <Title :grade="grade[0]"></Title>
+                    <Title :grade="data[actualGrade]"></Title>
                     <Indicator class="ml-2"></Indicator>
                 </div>
                 
                 <div class="flex flex-wrap mt-3 justify-center md:justify-normal">
-                    <CardCourse :course="course" v-for="course in data" :key="course.course">
+                    <CardCourse :course="course" v-for="course in data[actualGrade].courses" :key="course">
                     </CardCourse>
                 </div>
 
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -6,6 +6,10 @@
     let nombre = "Greison Rey Castilla Carmona"
 
     let selected = ref(options[0])
+    import { actualGrade } from '../composables/useGrades';
+    import data from "../components/Cardcourse/courses.json"
+
+    console.log(data[0].name)
 
 </script>
 
@@ -25,12 +29,12 @@
         </div>
 
         <div class="flex-col grow">
-            <div class="flex" v-for="option in options" :key="option">
+            <div class="flex" v-for="option in data.length" :key="option">
             
                 <div class="p-2 flex text-white text-sm justify-center hover:bg-white hover:text-gray-700 transition 
                     duration-500 ease-in-out cursor-pointer min-w-full"
-                    :class="{' bg-white text-gray-700 ':selected===option} " @click="selected=option">
-                    <b :class="{'text-gray-700':selected===option}">{{ option }}</b>
+                    :class="{' bg-white text-gray-700 ':actualGrade===option-1} " @click="actualGrade=option-1">
+                    <b :class="{'text-gray-700':actualGrade===option-1}">{{ data[option-1].name }}</b>
                 </div>
             
             </div>
